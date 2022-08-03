5992e9bd3254ff744e14a144730be95b3b2aeb1a
BrunoRijo <bruno_rijo@hotmail.com>
2022-08-03 13:36:36 -0300

    Consumindo imagens da API Utlizando a biblioteca Picasso.

--- app/src/main/java/br/com/bruno/soccernews/ui/adapter/NewsAdapter.java
+++ app/src/main/java/br/com/bruno/soccernews/ui/adapter/NewsAdapter.java
@@ -6,6 +6,8 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.squareup.picasso.Picasso;
+
 import java.util.List;
 
 import br.com.bruno.soccernews.databinding.NewsItemBinding;
@@ -32,6 +34,10 @@ public class NewsAdapter extends RecyclerView.Adapter<NewsAdapter.ViewHolder> {
         News news = this.news.get(position);
         holder.binding.tvTittle.setText(news.getTitle());
         holder.binding.tvDesc.setText(news.getDescription());
+        Picasso.get()
+                .load(news.getImage())
+                .fit()
+                .into(holder.binding.imageView );
     }
 
     @Override
